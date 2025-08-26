a28cffdf19ff57958d0d83a33c89cd49d3f4540c
destys <desstyss@gmail.com>
2025-08-26 10:38:37 +0300

    fix:26.08.25

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useMemo } from "react";
+import { Loader2Icon } from "lucide-react";
+
 import { DataTable } from "@/components/data-table"
 import { OrdersCard } from "@/components/orders/orders-card";
 import { ordersColumns } from "@/components/orders/orders-columns"
@@ -9,21 +12,29 @@ import { useUsers } from "@/hooks/use-users";
 import { useAuth } from "@/providers/auth-provider";
 
 export default function Page() {
-  const { data, updateOrder, deleteOrder } = useOrders(1, 50);
+  const { user, roleId } = useAuth();
+
+  // если роль — мастер (1), показываем только его заказы
+  const filters = useMemo(() => {
+    if (roleId === 1 && user?.id) {
+      return { $and: [{ master: { $eq: user.id } }] };
+    }
+    return {};
+  }, [roleId, user?.id]);
+
+  const { data, updateOrder, deleteOrder } = useOrders(1, 50, filters);
   const { users } = useUsers(1, 50);
 
-  const { roleId } = useAuth();
+  if (!user || !roleId) return <div className="flex justify-center items-center h-96"><Loader2Icon className="animate-spin" /></div>
 
   return (
     <div className="flex flex-col gap-4 md:gap-6">
-
       {roleId === 3 && <SectionCards />}
 
       <DataTable
         data={data}
-        columns={ordersColumns(users, updateOrder, deleteOrder)}
-        cardComponent={({ data }) => <OrdersCard
-          data={data} />}
+        columns={ordersColumns(users, updateOrder, deleteOrder, undefined, roleId, user)}
+        cardComponent={({ data }) => <OrdersCard data={data} />}
       />
     </div>
   )
